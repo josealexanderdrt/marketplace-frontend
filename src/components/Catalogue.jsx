12cd7479e0b4b7b1f5f9a622c0ae5125b38bc590
josealexanderdrt <josealexanderdrt@gmail.com>
2024-03-02 16:23:49 -0300

    home page alexander

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Carousel, Button } from "react-bootstrap";
+import aPic from "../assets/image/lam-home.png";
+import bPic from "../assets/image/sharge-disk-home.png";
+import cPic from "../assets/image/smart-watch-home.png";
+import dPic from "../assets/image/smart-speaker-home.png";
+import "../components/Catalogue.css";
 
 const Catalogue = () => {
   return (
     <div>
-        <h4>pagina principal, donde van fotos y el button ir a productos</h4>
-        {/* PUEDE IR FOOTER ACA O EN HOME O SI QUIERES CREAS UNA VISTA FOOTER */}
-    </div>
-  )
-}
+      <Carousel data-bs-theme="dark" slide interval={10000}>
+        <Carousel.Item>
+          <img className="d-flex w-100" src={aPic} alt="first Image" />
+          <Carousel.Caption>
+            <div className="divgotoproductusbutton">
+              <Button variant="outline-light" className="gotoproductusbutton">
+                Ir a Productos
+              </Button>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
 
-export default Catalogue
+        <Carousel.Item>
+          <img className="d-flex w-100" src={bPic} alt="second Image" />
+          <Carousel.Caption>
+            <h3>SHARGE Disk</h3>
+            <Button variant="outline-dark">Ampliar detalles</Button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-flex w-100" src={cPic} alt="third Image" />
+          <Carousel.Caption>
+            <h3>SmartWatch</h3>
+            <Button variant="outline-dark">Ampliar detalles</Button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-flex w-100" src={dPic} alt="quart Image" />
+          <Carousel.Caption>
+            <h3>SmartSpeaker</h3>
+            <Button variant="outline-dark">Ampliar detalles</Button>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Catalogue;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,7 +3,6 @@ import Catalogue from "../components/Catalogue";
 const Home = () => {
   return (
     <div>
-       
         <Catalogue />  
     </div>
   )
