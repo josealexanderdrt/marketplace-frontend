6aca29e5134207266f77d593095e8a85178407ec
NertaiL <hans.latoja@gmail.com>
2024-03-03 14:49:19 -0300

    autentication with google successful

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,42 @@
 import { Link } from "react-router-dom";
 import cubos from "../../src/assets/image/cubos.png";
+/* import userIcon from "../../src/assets/image/userIcon.png"; */ // Importa tu icono de usuario aquí
+import { useAuth0 } from "@auth0/auth0-react"; // Importa el hook de autenticación de Auth0
 import "./Navigation.css";
+
 const Navigation = () => {
+  const { isAuthenticated, user, logout } = useAuth0(); // Obtiene el estado de autenticación y la información del usuario
+
   return (
     <nav className="nav_style">
       <Link to="/">
         <img className="cubos1" src={cubos} alt="cubos" />
       </Link>
       <div className="register_login">
-      <Link className="access_nav" to="/auth_user">Iniciar</Link> | <Link className="register_nav" to="/users">Registrar</Link>
+        {isAuthenticated ? ( // Si el usuario está autenticado
+          <>
+            {/* <img src={userIcon} alt="user" className="user_icon" /> */}
+            <span>{user.name}</span>
+            <button
+              onClick={() => logout({ returnTo: "http://localhost:5173/" })}
+            >
+              Cerrar sesión
+            </button>
+          </>
+        ) : (
+          <>
+            <Link className="access_nav" to="/auth_user">
+              Iniciar sesión
+            </Link>{" "}
+            |{" "}
+            <Link className="register_nav" to="/users">
+              Registrar
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
 };
+
 export default Navigation;
