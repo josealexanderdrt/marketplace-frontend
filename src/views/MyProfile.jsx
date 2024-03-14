d12a95de0f377935e227f997e0499331d0686ff2
jean barraez <jeanbarraez@users.noreply.github.com>
2024-03-14 20:45:32 -0300

    se modifica src:components:allproductsComponents.css y jsx, Favorite(sin impacto)Footerlam.jsx y se crea su css, vistas Allproducts y MyProfile

--- src/views/MyProfile.jsx
+++ src/views/MyProfile.jsx
@@ -3,7 +3,6 @@ import { Container } from "react-bootstrap";
 import icons_user from "../../src/assets/image/icons_user.png";
 import { useContext } from "react";
 import { StoreContext } from "../context/StoreContext";
-import Footerlam from "../components/Footerlam";
 
 const MyProfile = () => {
   const { username } = useContext(StoreContext);
@@ -15,7 +14,6 @@ const MyProfile = () => {
         <h2>¡Hola! {username}</h2>
       </div>
       <Gallery />
-      <Footerlam/>
     </Container>
   );
 };
