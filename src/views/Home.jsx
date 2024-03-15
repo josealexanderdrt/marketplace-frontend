2fb54b76ed1ae6b047e4d3d508b9a117a75de328
NertaiL <hans.latoja@gmail.com>
2024-03-15 19:10:44 -0300

    testeando

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import Catalogue from "../components/Catalogue.jsx";
 import Footerlam from "../components/Footerlam.jsx";
 import Transition from "../components/Transition.jsx";
-import AllproductsComponent from "../components/AllproductsComponent.jsx";
+import CardsHome from "../components/HomePage.jsx";
 
 const Home = () => {
   return (
     <div>
       <Catalogue />
-      <Transition/>
-      <AllproductsComponent isHomePage={false} isFilterDescrip={false} isFilterBrand={false} /* numCards={8} */ columnClass="row-cols-md-4"/>
-      <Footerlam/>
+      <Transition />
+      <CardsHome
+        isHomePage={false}
+        isFilterDescrip={false}
+        isFilterBrand={false}
+        /* numCards={8} */ columnClass="row-cols-md-4"
+      />
+      <Footerlam />
     </div>
   );
 };
