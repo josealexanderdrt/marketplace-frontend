1f6f022cd3840934bfcbfb197b3e6079ca5e09a2
josealexanderdrt <josealexanderdrt@gmail.com>
2024-03-16 23:35:29 -0300

    cambios para miprofile responsive

--- src/views/MyProfile.jsx
+++ src/views/MyProfile.jsx
@@ -1,24 +1,13 @@
 import Gallery from "../components/Gallery";
 import { Container } from "react-bootstrap";
-import icons_user from "../../src/assets/image/icons_user.png";
-import { useContext } from "react";
+
 //import { StoreContext } from "../context/StoreContext";
-import { UserContext } from "../context/UserContext";
 
 const MyProfile = () => {
   //const { username } = useContext(StoreContext);
-  const { username } = useContext(UserContext);
 
   return (
     <Container className="view_profile">
-      <div className="icons_user_style">
-        <img
-          src={icons_user}
-          alt="user"
-          style={{ width: "200px", height: "200px" }}
-        />
-        <h2>¡Hola! {username}</h2>
-      </div>
       <Gallery />
     </Container>
   );
