cb1d1f293341258cb02e6e642780faaf5c65261e
NertaiL <hans.latoja@gmail.com>
2024-04-21 21:08:44 -0400

    modify connectRoutes

--- src/routes/ConnectRoutes.jsx
+++ src/routes/ConnectRoutes.jsx
@@ -21,7 +21,7 @@ const ConnectRoutes = () => {
       <R path="/" element={<Home />} />
       <R path="/auth_user" element={<Login />} />
       <R path="/users" element={<Register />} />
-      <R element={<RouteProtection username={username} />}>
+      <R element={<RouteProtection username={username} userId={userId} />}>
         <R path="/addproduct" element={<AddProduct />} />
         <R path="/profile/:id" element={<MyProfile />} />
         <R path="/favorite/:id" element={<MyFavorite />} />
